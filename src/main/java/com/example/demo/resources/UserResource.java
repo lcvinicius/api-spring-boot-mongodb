aae3582faf8eb536789778d1dd4a7cede5207159
Lucas Vinícius <lucasvsn19@gmail.com>
2024-05-14 20:03:20 -0300

    feat: Getting a user by id

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,32 +1,48 @@
 package com.example.demo.resources;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
 import com.example.demo.dto.UserDTO;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
 public class UserResource {
-	
-   @Autowired
-   private UserService userService;
-	
-	@GetMapping
-	public ResponseEntity<List<UserDTO>> findAll(){
-		
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/users")
+	public ResponseEntity<List<UserDTO>> findAll() {
+
 		List<User> list = userService.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-	
-		return ResponseEntity.ok().body(listDto);
 
-}
+		return ResponseEntity.ok().body(listDto);
 	}
+
+	@GetMapping("/users/{id}")
+	public ResponseEntity<Object> findById(@PathVariable String id) {
+
+		Optional<User> obj = userRepository.findById(id);
+
+		 if(obj.isEmpty()){
+	            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+	        }
+		return ResponseEntity.status(HttpStatus.OK).body(obj.get());
+
+	}
+}
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,14 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public Optional<User> findById(String id) {
+		
+		Optional<User> user = userRepository.findById(id);
+		
+		
+		return user;
 	}
 
 }
